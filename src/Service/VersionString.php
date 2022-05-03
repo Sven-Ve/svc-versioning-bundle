fbd7597c1247030c31d8ffda9dafa72950f98dea
Sven Vetter <git.09o@svenvetter.com>
2022-05-03 13:28:46 +0200

    fix syntax error

--- src/Service/VersionString.php
+++ src/Service/VersionString.php
@@ -1,45 +1,47 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Svc\VersioningBundle\Service;
 
 class VersionString
 {
-    private const VERSION_SEPARATOR = '.';
+  private const VERSION_SEPARATOR = '.';
 
-    public function versionStringToVersionArray(string $versionString): array
-    {
-      $versionArray = $this->parser($versionString);
+  public function versionStringToVersionArray(string $versionString): array
+  {
+    $versionArray = $this->parser($versionString);
 
-        return [
-            'major' => intval($versionArray[0]),
-            'minor' => intval($versionArray[1]),
-            'patch' => intval($versionArray[2]),
-        ];
-    }
+    return [
+      'major' => intval($versionArray[0]),
+      'minor' => intval($versionArray[1]),
+      'patch' => intval($versionArray[2]),
+    ];
+  }
 
 
-    public function getInitial(): string
-    {
-      $format = '0%s0%s1';
-      $ret    = sprintf($format, self::VERSION_SEPARATOR, self::VERSION_SEPARATOR);
+  public function getInitial(): string
+  {
+    $format = '0%s0%s1';
+    $ret    = sprintf($format, self::VERSION_SEPARATOR, self::VERSION_SEPARATOR);
 
-      return $ret;
-    }
+    return $ret;
+  }
 
-    public function versionArrayToVersionString(array $versionArray): string
-    {
-      return implode(self::VERSION_SEPARATOR, $versionArray);
-    }
+  public function versionArrayToVersionString(array $versionArray): string
+  {
+    return implode(self::VERSION_SEPARATOR, $versionArray);
+  }
 
-    /**
-     * convert version string (1.2.3) in an array
-     *
-     * @param string $version
-     * @return array
-     */
-    private function parser(string $version): array
-      $parts = explode('.', $version);
-      return $parts;
-    }
+  /**
+   * convert version string (1.2.3) in an array
+   *
+   * @param string $version
+   * @return array
+   */
+  private function parser(string $version): array
+  {
+    $parts = explode('.', $version);
+    return $parts;
+  }
 }
